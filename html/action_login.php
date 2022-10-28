a2b78b218f9e8916cb1c4e11ffbb9539b1657f02
unaiayucar <uayucar@aliasrobotics.com>
2022-10-28 18:07:14 +0200

    v1 on its process

--- html/action_login.php
+++ html/action_login.php
@@ -1,8 +1,30 @@
-<!DOCTYPE html>
-<html>
-<body>
-    <?php
-    echo "Hello World!";
-    ?>
-</body>
-</html>
+<?php
+    ob_start();
+    if (isset($_POST['uname'])){
+        if ($_POST['uname'] == "unai" ){
+            if (isset($_POST['psw'])){
+                if ($_POST['psw'] == "unai" ){
+                    $link='main_unai/main.html';
+                    header('location: '.$link); 
+                    exit;
+                } else {
+                    $link='index.html';
+                    header('location: '.$link); 
+                    exit;
+                }
+            } else {
+                $link='index.html';
+                header('location: '.$link); 
+                exit;
+            }
+        } else {
+            $link='index.html';
+            header('location: '.$link); 
+            exit;
+        }
+    } else {
+        $link='index.html';
+        header('location: '.$link); 
+        exit;
+    }
+?>
